5f576fe9aeb4fb0f68953d213eb5a7a874bb3ade
Dmitry Sheberstov <dsheberstov@aq.ru>
2026-05-29 20:10:28 +0300

    Выполнение шага 2: Добавление опции --format для выбора формата

--- cmd/gendiff/main.go
+++ cmd/gendiff/main.go
@@ -10,6 +10,15 @@ func main() {
 	cmd := &cli.Command{
 		Name:		"gendiff",
 		Usage:		"Compares two configuration files and shows a difference",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:		"format",
+				Aliases:	[]string{"f"},
+				DefaultText:	"stylish",
+                		Value:		"stylish",
+                		Usage:		"output format",
+			},
+		},
 		Action: func(_ context.Context, cmd *cli.Command) error {
 			return nil
 		},
